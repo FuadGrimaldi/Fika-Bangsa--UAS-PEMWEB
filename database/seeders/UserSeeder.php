7c0ba6d96e5f0ef5d37a05e3bed72eadf75d07cf
Fuad Grimaldi <112310926+FuadGrimaldi@users.noreply.github.com>
2024-01-08 10:51:47 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,24 +16,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([[
-            'name' => 'Admin',
-            'email' => 'fuadgrimaldi14@gmail.com',
-            'password' => Hash::make('fuadgrimaldi'),
-            'phone_number' => '087823119321',
-            'avatar' => '',
-            'role' => 'admin',
-            'created_at' => now(),
-            'updated_at' => now() 
-        ],[
-            'name' => 'Admin2',
-            'email' => 'rere@gmail.com',
-            'password' => Hash::make('123rere'),
-            'phone_number' => '087823119321',
-            'avatar' => '',
-            'role' => 'admin',
-            'created_at' => now(),
-            'updated_at' => now() 
-        ]]);
+        // 
     }
 }
